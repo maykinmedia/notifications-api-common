724331b6a78066e491c7c5071511910cc5e943f6
Steven Bal <steven@maykinmedia.nl>
2022-07-04 14:38:02 +0200

    :tada: Move initial code from vng_api_common.notifications

---
+++ notifications_api_common/api/views.py
@@ -0,0 +1,70 @@
+from django.conf import settings
+from django.utils.module_loading import import_string
+
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from ...permissions import AuthScopesRequired
+from ...scopes import Scope
+from ...serializers import FoutSerializer, ValidatieFoutSerializer
+from ..constants import SCOPE_NOTIFICATIES_PUBLICEREN_LABEL
+from .serializers import NotificatieSerializer
+
+
+class NotificationBaseView(APIView):
+    """
+    Abstract view to receive webhooks
+    """
+
+    swagger_schema = None
+
+    permission_classes = (AuthScopesRequired,)
+    required_scopes = Scope(
+        SCOPE_NOTIFICATIES_PUBLICEREN_LABEL
+    )  # FIXME: this should be standalone!
+
+    def get_serializer(self, *args, **kwargs):
+        return NotificatieSerializer(*args, **kwargs)
+
+    @swagger_auto_schema(
+        responses={
+            204: "",
+            400: ValidatieFoutSerializer,
+            401: FoutSerializer,
+            403: FoutSerializer,
+            429: FoutSerializer,
+            500: FoutSerializer,
+            502: FoutSerializer,
+            503: FoutSerializer,
+        },
+        operation_id="notification_receive",
+        tags=["Notificaties"],
+    )
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.handle_notification(serializer.validated_data)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def handle_notification(self, message):
+        raise NotImplementedError("You must implemented `handle_notification`")
+
+
+class NotificationView(NotificationBaseView):
+    action = "create"
+    permission_classes = (AuthScopesRequired,)
+    required_scopes = {"create": Scope(SCOPE_NOTIFICATIES_PUBLICEREN_LABEL)}
+
+    def create(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
+    def handle_notification(self, message: dict) -> None:
+        _handler = getattr(
+            settings,
+            "DEFAULT_NOTIFICATIONS_HANDLER",
+            "vng_api_common.notifications.handlers.default",
+        )
+        handler = import_string(_handler)
+        handler.handle(message)
---
+++ notifications_api_common/handlers.py
@@ -0,0 +1,65 @@
+import logging
+
+from djangorestframework_camel_case.util import underscoreize
+
+from ..authorizations.models import Applicatie
+from ..authorizations.serializers import ApplicatieUuidSerializer
+from ..client import get_client
+from ..constants import CommonResourceAction
+from ..utils import get_uuid_from_path
+from .constants import KANAAL_AUTORISATIES
+
+
+class LoggingHandler:
+    def handle(self, message: dict) -> None:
+        logger = logging.getLogger("notifications")
+        logger.info("Received notification %r", message)
+
+
+class AuthHandler:
+    def _request_auth(self, url: str) -> dict:
+        client = get_client(url)
+        response = client.retrieve("applicatie", url)
+        return underscoreize(response)
+
+    def handle(self, message: dict) -> None:
+        uuid = get_uuid_from_path(message["resource_url"])
+
+        if message["actie"] == CommonResourceAction.destroy:
+            Applicatie.objects.filter(uuid=uuid).delete()
+            return
+
+        # get info
+        applicatie_data = self._request_auth(message["resource_url"])
+        applicatie_data["uuid"] = uuid
+
+        # update models
+        try:
+            applicatie = Applicatie.objects.get(uuid=uuid)
+        except Applicatie.DoesNotExist:
+            applicatie_serializer = ApplicatieUuidSerializer(data=applicatie_data)
+        else:
+            applicatie_serializer = ApplicatieUuidSerializer(
+                applicatie, data=applicatie_data
+            )
+        applicatie_serializer.is_valid()
+        applicatie_serializer.save()
+
+
+class RoutingHandler:
+    def __init__(self, config: dict, default=None):
+        self.config = config
+        self.default = default
+
+    def handle(self, message: dict):
+        handler = self.config.get(message["kanaal"])
+        if handler is not None:
+            handler.handle(message)
+        elif self.default:
+            self.default.handle(message)
+
+
+log = LoggingHandler()
+auth = AuthHandler()
+
+default = RoutingHandler({KANAAL_AUTORISATIES: auth}, default=log)
