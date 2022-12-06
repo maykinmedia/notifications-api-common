962c8d62b28540c4d30cfa244457257c4e679cdb
Sergei Maertens <sergei@maykinmedia.nl>
2022-12-06 11:41:51 +0100

    :art: Remove deprecated default_app_config

--- notifications_api_common/apps.py
+++ notifications_api_common/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class NotificationsApiCommonConfig(AppConfig):
     name = "notifications_api_common"
+    verbose_name = _("Notifications API integration")
