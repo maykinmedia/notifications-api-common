ce300c43d9a818853736ba01fd894d9d6a8429b9
Sergei Maertens <sergei@maykinmedia.nl>
2022-12-13 09:16:02 +0100

    :card_file_box: Migrate auto field to BigAutoField
    This is going to be the default in future Django versions

--- notifications_api_common/apps.py
+++ notifications_api_common/apps.py
@@ -5,3 +5,4 @@ from django.utils.translation import gettext_lazy as _
 class NotificationsApiCommonConfig(AppConfig):
     name = "notifications_api_common"
     verbose_name = _("Notifications API integration")
+    default_auto_field = "django.db.models.BigAutoField"
--- testapp/settings.py
+++ testapp/settings.py
@@ -17,6 +17,8 @@ DATABASES = {
     }
 }
 
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+
 INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.auth",
