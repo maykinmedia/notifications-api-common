32630470b0e34603408b0df3d95fd1b197ec8e1e
Steven Bal <steven@maykinmedia.nl>
2024-09-20 16:44:09 +0200

    :sparkles: Add support for py3.11/3.12 and drop support for 3.8/3.9

--- testapp/settings.py
+++ testapp/settings.py
@@ -2,6 +2,8 @@ import os
 
 DEBUG = True
 
+USE_TZ = False
+
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 SECRET_KEY = "so-secret-i-cant-believe-you-are-looking-at-this"
@@ -63,4 +65,4 @@ TEMPLATES = [
 
 ROOT_URLCONF = "testapp.urls"
 
-ZGW_CONSUMERS_CLIENT_CLASS = "zgw_consumers.client.ZGWClient"
+ZGW_CONSUMERS_CLIENT_CLASS = "zgw_consumers.legacy.client.ZGWClient"
