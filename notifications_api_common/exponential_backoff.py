886d3f7487b3d1f37f43299711773e5a01f35678
Tim de Beer <tim@maykinmedia.nl>
2025-07-07 10:05:43 +0200

    :bulb: [open-zaak/open-notificaties#290] add celercy comment

--- notifications_api_common/exponential_backoff.py
+++ notifications_api_common/exponential_backoff.py
@@ -4,7 +4,12 @@ import random
 def get_exponential_backoff_interval(
     factor: int, retries: int, maximum: int, base: int, full_jitter: bool = False
 ) -> int:
-    """Calculate the exponential backoff wait time."""
+    """
+    Calculate the exponential backoff wait time.
+
+    This function is taken from Celery but overridden here to make sure the base factor is configurable.
+    (https://docs.celeryq.dev/en/latest/_modules/celery/utils/time.html#get_exponential_backoff_interval)
+    """
     # Will be zero if factor equals 0
     countdown = min(maximum, factor * (base**retries))
     # Full jitter according to
